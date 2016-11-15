6ebce3cd92ee8b854085942886e1c08b34eb619c
Christopher Wong <chriskwong91@gmail.com>
2016-11-15 12:27:40 -0800

    Fixed type in index.js

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,5 +1,17 @@
 import { combineReducers } from 'redux';
-impport { SUBREDDIT_INPUT} from '../actions/index';
+import { SUBREDDIT_INPUT} from '../actions/index';
+
+// const rootReducer = combineReducers({
+//   state: (state = {}) => state,
+// });
+function subredditInput (state = '', action) {
+  switch(action.type) {
+    case SUBREDDIT_INPUT:
+      return action.text;
+    default:
+      return state;
+  }
+}
 
 const rootReducer = combineReducers({
   state: (state = {}) => state,
